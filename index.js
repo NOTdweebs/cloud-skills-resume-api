542a8286dd559f6539f3c2629ae65f4756e90888
NOTdweebs <mailapplezsfw@gmail.com>
2025-05-31 11:46:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const AWS = require("aws-sdk");
 const db = new AWS.DynamoDB.DocumentClient();
-const TABLE_NAME = process.env.TABLE_NAME; //test
+const TABLE_NAME = process.env.TABLE_NAME;//test
 
 exports.handler = async () => {
   try {
